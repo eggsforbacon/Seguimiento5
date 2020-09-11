d275f426edf373a762cab38da51bcaf4a660cbb2
Samuel Hernandez <doicirohangnan@gmail.com>
2020-09-11 11:16:42 -0500

    Functional version. findIntersect is not at its final state

--- src/model/Operations.java
+++ src/model/Operations.java
@@ -3,7 +3,7 @@ package model;
 import java.util.*;
 import java.io.IOException;
 public class Operations {
-  static String[] ORDINALS = {"primer","segundo","tercer"};
+  final static String[] ORDINALS = {"primer","segundo","tercer"};
   public static int[][] setArrays(Scanner in) {
     System.out.println("Ingrese el tamanio del primer arreglo: ");
     int size = in.nextInt();
@@ -22,12 +22,36 @@ public class Operations {
   }
 
   public static int[] fillArray(Scanner in, int[] currentArray, int next) {
-    System.out.println("Ingrese los elementos del " + ORDINALS[next]);
+    System.out.println("Ingrese los elementos del " + ORDINALS[next] + " arreglo:\n");
     for (int i = 0; i < currentArray.length; i++) {
-      System.out.println("Ingrese el elemnto #" + i);
+      System.out.println("Ingrese el elemnto #" + (i+1));
       currentArray[i] = in.nextInt();
       in.nextLine();
     }
     return currentArray;
+  }
+
+  public static int[] findIntersect(int[] arr1, int[] arr2, int[] arr3) {
+    int[] intersect = new int[1];
+    int index = 0;
+    for (int j = 0; j < arr1.length; j++) {
+      for (int k = 0; k < arr2.length; k++) {
+        for (int l = 0; l < arr3.length; l++) {
+          flag = Arrays.asList(intersect).contains(arr1[j]);
+          if (arr1[j] == arr2[k] && arr2[k] == arr3[l]) {
+            intersect[index] = arr1[j];
+            intersect = Arrays.copyOf(intersect, intersect.length + 1);
+            index++;
+          }
+        }
+      }
+    }
+    intersect = Arrays.copyOf(intersect, intersect.length -1);
+
+    return intersect;
+  }
+
+  public static int[] formatAndPrint(int[] arr) {
+    
   }
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -10,7 +10,16 @@ public class Main {
     int[] arr1 = arrGroup[0];
     int[] arr2 = arrGroup[1];
     int[] arr3 = arrGroup[2];
-    
+
+    int next = 0;
+    arr1 = Operations.fillArray(in,arr1,next);
+    next++;
+    arr2 = Operations.fillArray(in,arr2,next);
+    next++;
+    arr3 = Operations.fillArray(in,arr3,next);
+
+    int[] intersect = Operations.findIntersect(arr1,arr2,arr3);
+    System.out.println("El arreglo interseccion es:\n" + Arrays.toString(intersect));
   }
 
 }
