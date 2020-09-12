c791e81b2c2a24328a50eb235e8034efa05e48bd
Samuel Hernandez <doicirohangnan@gmail.com>
2020-09-11 23:00:42 -0500

    Full functionality of every function and included clear screen method

--- src/model/Operations.java
+++ src/model/Operations.java
@@ -8,26 +8,31 @@ public class Operations {
     System.out.println("Ingrese el tamanio del primer arreglo: ");
     int size = in.nextInt();
     in.nextLine();
-    int[] arr1 = new int[size];
+    int[] arr1 = new int[size]; //Entrada, guarda el primer arreglo
+    clsm();
     System.out.println("Ingrese el tamanio del segundo arreglo: ");
     size = in.nextInt();
     in.nextLine();
-    int[] arr2 = new int[size];
+    int[] arr2 = new int[size]; //Entrada, guarda el segundo arreglo
+    clsm();
     System.out.println("Ingrese el tamanio del tercer arreglo: ");
     size = in.nextInt();
     in.nextLine();
-    int[] arr3 = new int[size];
-    int[][] arrGroup = {arr1,arr2,arr3};
+    int[] arr3 = new int[size]; //Entrada, guarda el tercer arreglo
+    clsm();
+    int[][] arrGroup = {arr1,arr2,arr3}; //Entrada, le pasa todos los areglos en una matriz al main
     return arrGroup;
   }
 
   public static int[] fillArray(Scanner in, int[] currentArray, int next) {
-    System.out.println("Ingrese los elementos del " + ORDINALS[next] + " arreglo:\n");
     for (int i = 0; i < currentArray.length; i++) {
+      System.out.println("Ingrese los elementos del " + ORDINALS[next] + " arreglo:\n");
       System.out.println("Ingrese el elemnto #" + (i+1));
       currentArray[i] = in.nextInt();
       in.nextLine();
+      clsm();
     }
+    clsm();
     return currentArray;
   }
 
@@ -37,7 +42,6 @@ public class Operations {
     for (int j = 0; j < arr1.length; j++) {
       for (int k = 0; k < arr2.length; k++) {
         for (int l = 0; l < arr3.length; l++) {
-          flag = Arrays.asList(intersect).contains(arr1[j]);
           if (arr1[j] == arr2[k] && arr2[k] == arr3[l]) {
             intersect[index] = arr1[j];
             intersect = Arrays.copyOf(intersect, intersect.length + 1);
@@ -47,11 +51,32 @@ public class Operations {
       }
     }
     intersect = Arrays.copyOf(intersect, intersect.length -1);
+    intersect = formatArray(intersect);
 
-    return intersect;
+    return intersect; //Salida, le pasa el intersect al main con el formato adecuado
   }
 
-  public static int[] formatAndPrint(int[] arr) {
-    
+  private static int[] formatArray(int[] arr) { //formatea el array de interseccion
+    Arrays.sort(arr);
+    for (int m = 0; m < arr.length; m++) {
+      if (arr[m] == arr[m+1]) {
+        arr[m+1] = 0;
+        for (int n = m+1; n < arr.length -1; n++) {
+          arr[n] = arr[n+1];
+        }
+        arr = Arrays.copyOf(arr, arr.length -1);
+      }
+    }
+
+    return arr;
+  }
+
+  public static void clsm() {
+    try {
+      new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor();
+    }
+    catch (Exception e) {
+      System.out.println(e);
+    }
   }
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -5,11 +5,12 @@ import java.util.*;
 import java.io.IOException;
 public class Main {
   public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    int[][] arrGroup = Operations.setArrays(in);
-    int[] arr1 = arrGroup[0];
-    int[] arr2 = arrGroup[1];
-    int[] arr3 = arrGroup[2];
+    Scanner in = new Scanner(System.in); //Entrada, escaner
+    Operations.clsm();
+    int[][] arrGroup = Operations.setArrays(in); //Entrada, arreglo 3x{x,y,z} que contiene los arreglos
+    int[] arr1 = arrGroup[0]; //Entrada, arreglos que
+    int[] arr2 = arrGroup[1]; //serán intersectados
+    int[] arr3 = arrGroup[2]; //
 
     int next = 0;
     arr1 = Operations.fillArray(in,arr1,next);
@@ -18,7 +19,7 @@ public class Main {
     next++;
     arr3 = Operations.fillArray(in,arr3,next);
 
-    int[] intersect = Operations.findIntersect(arr1,arr2,arr3);
+    int[] intersect = Operations.findIntersect(arr1,arr2,arr3); //Salida, arreglo de intersección
     System.out.println("El arreglo interseccion es:\n" + Arrays.toString(intersect));
   }
 
